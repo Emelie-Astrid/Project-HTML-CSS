23ab602c5dd48cb3cfe0e205050e1bb152916834
Emelie <Emelie.almlof@gmail.com>
2022-12-11 23:51:09 +0100

    new assets, changes to navigation, styling review and about page

--- index.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    let x = document.querySelector("#myTopnav");
-    if (x.className === "navigation") {
-      x.className += " responsive";
-    } else {
-      x.className = "navigation";
-    }
-  }
